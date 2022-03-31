d200fe27bf9365e010b878f0e6ac4966422eb42f
sayamkhanraj <snsayamkhanraj@gmail.com>
2022-03-31 22:46:42 +0600

    add couter

--- src/Pages/Home/Counter/Counter.js
+++ src/Pages/Home/Counter/Counter.js
@@ -5,18 +5,23 @@ const Counter = () => {
                   <div className="counter my-5">
                         <div className="d-flex align-items-center justify-content-around count-style">
                                <div className="counter-border">
+                                    <div className="text">
                                     <h1>487</h1>
                                     <p>MATCHES PLAYED</p>
-                                      </div>  
-                               <div>
+                                    </div>
+                                    <div className="line">
+
+                                    </div>
+                              </div>  
+                               <div className="counter-border">
                                     <h1>310</h1>
                                     <p>WINS</p>
                                       </div>  
-                               <div>
+                               <div className="counter-border">
                                     <h1>27</h1>
                                     <p>TEAM MEMBERS</p>
                                       </div>  
-                               <div>
+                               <div className="counter-border">
                                     <h1>14</h1>
                                     <p>AWARDS WON</p>
                                       </div>  
